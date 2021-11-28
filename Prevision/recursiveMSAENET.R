a401f00f7f9f7580ef338788f06ac1fe7c12e965
Kyllien Romand <91131288+Kyllien@users.noreply.github.com>
2021-11-28 17:08:47 +0100

    Prevision 2018-2019-2020

--- Prevision/recursiveMSAENET.R
+++ Prevision/recursiveMSAENET.R
@@ -5,6 +5,7 @@ library(glmnet)
 library(msaenet)
 library(Metrics)
 library(MLmetrics)
+library(ggplot2)
 
 setwd("C:/Users/33640/OneDrive/Bureau/DRIM")
 dlbase <- read.csv("base_diff.csv")
@@ -22,24 +23,42 @@ depvar <- data.frame(dlbase) %>%
   as.matrix()
 
 # Nowcasting based on adaptive elastic-net
-# One-step ahead forecasts for h=12
-# forecast horizon h=12
-for (i in 1:12) {
-  x <- data.matrix(depvar[1:(106+i),])	
-  y <- data.matrix(indepvar[1:(106+i)])
+# One-step ahead forecasts for h=24
+# forecast horizon h=24
+#Prevision de 2018 à 2019
+for (i in 1:24) {
+  x <- data.matrix(depvar[1:(95+i),])	
+  y <- data.matrix(indepvar[1:(95+i)])
   
   model_cv <- aenet(x, y, family = "gaussian", init = "ridge", alphas = 0.5, tune = "cv", nfolds = 10, seed = 1001)
-  pred <- c(pred, predict(model_cv, newx = depvar[(107+i),]))	# one-step ahead forecast 
+  pred <- c(pred, predict(model_cv, newx = depvar[(96+i),]))	# one-step ahead forecast 
 }
 pred
 model_cv$beta
 
-rmse(y[c(107:118)],pred)
-mse(y[c(107:118)],pred)
-mae(y[c(107:118)],pred)
+rmse(y[c(96:119)],pred)
+mse(y[c(96:119)],pred)
+mae(y[c(96:119)],pred)
 
-MAPE(y[c(107:118)],pred)   #library(MLmetrics)
-mape(y[c(107:118)],pred)   #library Metrics
+MAPE(y[c(96:119)],pred)   #library(MLmetrics)
+mape(y[c(96:119)],pred)   #library Metrics
+
+MAPE(y[c(96:107)],pred[c(1:12)]) #Pour l'année 2018
+MAPE(y[c(108:119)],pred[c(13:24)]) #POur l'année 2019
+
+#Prevision pour 2020
+for (i in 1:12) {
+  x <- data.matrix(depvar[1:(118+i),])	
+  y <- data.matrix(indepvar[1:(118+i)])
+  
+  model_cv <- aenet(x, y, family = "gaussian", init = "ridge", alphas = 0.5, tune = "cv", nfolds = 10, seed = 1001)
+  pred <- c(pred, predict(model_cv, newx = depvar[(119+i),]))	# one-step ahead forecast
+  indepvar[119+i] <- pred[24+i]
+}
+pred
+
+plot(pred)
+
 
 write(t(pred),file="forecast_aenet.txt",ncolumn=1,append=FALSE)
 
