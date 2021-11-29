acc42eb98753de8b8cd855113326fcbaece1d98d
Kyllien Romand <91131288+Kyllien@users.noreply.github.com>
2021-11-29 18:53:34 +0100

    Avec +12

--- Gestion Data/Stationnarisation.R
+++ Gestion Data/Stationnarisation.R
@@ -18,12 +18,16 @@ baseAll$Date <- as.Date(baseAll$Date)
 
 Stationnarite <- function(df){
   #On met les deux premiere colonnes dans la seconde base car cest la variable à expliquer et la date
-  df_diff <- df[-1,c(1,2)]
+  df_diff <- df[1:120,c(1,2)]
   for (i in (3:length(df))){
     Cname <- colnames(df_diff)
     Cname <-append(Cname,colnames(df[i]))
     if(substr(colnames(df[i]),1,3) == "Tx_"){
-      df_diff<-cbind(df_diff,df[-1,i])
+      df2<-data_frame(mean(df[1:12,i]))
+      for(j in (2:120)){
+        df2<-rbind(df2,mean(df[j:j+11,i]))
+      }
+      df_diff<-cbind(df_diff,df2)
     }
     else if(substr(colnames(df[i]),1,3) == "Qt_"){
       m1 <- ar(diff(df[,i]),method="mle")
@@ -31,14 +35,14 @@ Stationnarite <- function(df){
       Test2 <- adfTest(df[,i], lag=m1$order, type="c")
       Test3 <- adfTest(df[,i], lag=m1$order, type="ct")
       if(Test1@test$p.value>0.05 | Test2@test$p.value>0.05 | Test3@test$p.value>0.05){
-        df_diff<-cbind(df_diff,diff(log(df[,i]),lag=1))
+        df_diff<-cbind(df_diff,diff(log(df[,i]),lag=12))
       }
       else{
-        df_diff<-cbind(df_diff,log(df[-1,i]))
+        df_diff<-cbind(df_diff,diff(df[,i],lag=12))
       }
     }
     else{
-      df_diff<-cbind(df_diff,diff(df[,i],lag=1))
+      df_diff<-cbind(df_diff,diff(df[,i],lag=12))
     }
     colnames(df_diff)<-Cname
   }
@@ -90,4 +94,21 @@ show(fit)
 # }
 # base_diff$Tx_Defaut <- adj
 
+library(stringr)
+vec <- c("LCI","Tx_interet_CT","Qt_Euribor1","Qt_Euribor2","Qt_FTSE","Qt_Enel","Tx_Pret_SNF","Qt_GT_credit_conso","Qt_M1","Qt_M2","Qt_M3","Tx_Fund_Raised","Tx_Loans_NonFinancial","Tx_Debt_CB","Qt_Debt_Monetary","Qt_Debt_Financial","Qt_Debt_Resident","Qt_Debt_NonResident","Qt_Debt_Gross","Qt_Stock_Gov")
+outlier <- function(df,vect){
+  for(i in vec){
+    print(i)
+    command <- str_c("df2 <- ts(df$",i,",start = c(2010,02), frequency=12)")
+    eval(parse(text=command))
+    fit <- tso(df2)
+    adj <- fit$yadj
+    command <- str_c("df$",i,"<- adj")
+    eval(parse(text=command))
+  }
+  return(df)
+}
+df3 <- outlier(base_diff,vec)
 write.csv(base_diff,"base_diff.csv")
+
+
